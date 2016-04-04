eaca68606254875e04bdc32e25187458d198b0e1
Arianna Marin <marin.arianna@gmail.com>
2016-04-03 21:59:41 -0700

    added input field for userfor range limit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,29 +1,35 @@
 // Place your FizzBuzz code here. 
+// A function is used so that action will occur on click
+function deployKyloRen() {
 
-// Count through the numbers 1-20 and find out if each number is divisible by 3, 5 or 3 and 5.
-for (i=1; i<=20; i=i+1){
-// For each number do the following check:
+	//variable for range limit based on user input
+	var num_end = document.getElementById('RangeLim').value;
 
-// Check if number is divisible by 3
-if (i % 3 === 0) {
-	// If so, see if it is ALSO divisible by 5
-	if (i % 5 === 0) {
-		// If it divisible by 3 and 5 return fizz buzz
-		console.log ('FizzBuzz');
-	} else {
-		// If it is only divisibile by 3 return fizz
-		console.log ('Fizz');
-	}		
-}	//Check if number is divisible by 5
-	else if (i % 5 === 0){
-	//If it is return buzz
-	console.log ('Buzz');
-	} else {
-	//Check if number is divisible neither
+	// Count through the numbers 1-20 and find out if each number is divisible by 3, 5 or 3 and 5.
+	for (i=1; i<= num_end; i=i+1){
+	// For each number do the following check:
 
-	//If so then return the number itself
-	console.log(i);
+	// Check if number is divisible by 3
+	if (i % 3 === 0) {
+		// If so, see if it is ALSO divisible by 5
+		if (i % 5 === 0) {
+			// If it divisible by 3 and 5 return fizz buzz
+			console.log ('KyloRen');
+		} else {
+			// If it is only divisibile by 3 return fizz
+			console.log ('Kylo');
+		}		
+	}	//Check if number is divisible by 5
+		else if (i % 5 === 0){
+		//If it is return buzz
+		console.log ('Ren');
+		} else {
+		//Check if number is divisible neither
+
+		//If so then return the number itself
+		console.log(i);
+		}
+
+
 	}
-
-
 }
